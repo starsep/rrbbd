ed6f1f702c332c1d5467c0bd14ab24da779e89a0
Filip Czaplicki <filipczaplicki@gmail.com>
2017-03-06 21:09:30 +0100

    Version from gradle.

--- src/main/java/com/starsep/rrbridge_bidding_data/core/Main.java
+++ src/main/java/com/starsep/rrbridge_bidding_data/core/Main.java
@@ -4,12 +4,20 @@ import com.starsep.rrbridge_bidding_data.translation.Translatable;
 import com.starsep.rrbridge_bidding_data.translation.Translation;
 import com.starsep.rrbridge_bidding_data.gui.MainGUI;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.Properties;
 
 public class Main {
     private static final String PROGRAM_NAME = "RRBridge Bidding Data";
+
+    private static Properties buildProperties;
+    private static final String BUILD_PROPERTIES_FILENAME = "build.properties";
+    private static final String VERSION_KEY = "version";
 
     public static final String NO_GUI_FLAG = "--no-gui";
     public static final String ENGLISH_FLAG = "--english";
@@ -92,7 +100,30 @@ public class Main {
         setLocale(new Locale("pl"));
     }
 
+    private static void initBuildProperties() {
+        buildProperties =  new Properties();
+        InputStream input = null;
+
+        try {
+            input = Main.class.getClassLoader().getResourceAsStream(BUILD_PROPERTIES_FILENAME);
+            buildProperties.load(input);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
     public static String getVersion() {
-        return "0.0.1";
+        if (buildProperties == null) {
+            initBuildProperties();
+        }
+        return buildProperties.getProperty(VERSION_KEY);
     }
 }
